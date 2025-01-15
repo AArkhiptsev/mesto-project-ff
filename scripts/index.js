1bca1063d2f74f88ab4855703f0706832af5fb9e
Alexander Arkhiptsev <a.arkhiptsev@gmail.com>
2025-01-15 19:43:37 +0300

    вторая редакция 15.01

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,13 @@ const cardsContainer = document.querySelector(".places__list");
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-function createCard(card) {
+function createCard(card, deleteButton) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   cardElement.querySelector(".card__image").src = card.link;
   cardElement.querySelector(".card__image").alt = card.alt;
   cardElement.querySelector(".card__title").textContent = card.name;
-
   const removeButton = cardElement.querySelector(".card__delete-button");
-  removeButton.addEventListener("click", () => deleteCard(removeButton));
+  removeButton.addEventListener("click", () => deleteCard(cardElement));
   return cardElement;
 }
 
@@ -24,7 +23,7 @@ function deleteCard(deleteButton) {
 // @todo: Вывести карточки на страницу
 function renderCards() {
   initialCards.forEach((card) => {
-    cardsContainer.append(createCard(card));
+    cardsContainer.append(createCard(card,deleteCard));
   });
 }
 
